5c330bb282270fa428b7a762d35ce37b1416ead0
Venus Poon <venuspsw@hotmail.com>
2024-01-30 12:41:12 +0100

    uploaded day1-3

---
+++ src/Day4_DuplicateEffort/Main.java
@@ -0,0 +1,18 @@
+package Day4_DuplicateEffort;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        DuplicateEffort de = new DuplicateEffort();
+        Path p = Paths.get("src/Day4_DuplicateEffort/Input");
+        de.readFromFile(p);
+        int totalPair = de.countFullyDuplicatePair(de.getAssigmentPair());
+        System.out.println("Total fully duplicate pair: " + totalPair);
+
+        int totalDuplicatePair = de.countDuplicatePair(de.getAssigmentPair());
+        System.out.println("Total duplicate pair: " + totalDuplicatePair);
+
+    }
+}
